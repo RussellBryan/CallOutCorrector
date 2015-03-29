16feeefff9c5e4f73e9a5a24de3bffb2a198e7a6
RussellBryan <rmbryan@ucdavis.edu>
2015-03-28 21:04:16 -0700

    Added Trial class for navigating files

--- ReadXml.py
+++ ReadXml.py
@@ -1,17 +1,46 @@
 import xml.etree.ElementTree as ET
 import os
 
-os.chdir('/Users/admin/Desktop/')
-tree=ET.parse('speech_test.xml')
-root=tree.getroot()
+all_data=os.listdir('/Users/admin/Desktop/datas')
 
-utters=root.findall("./turn/user_input/")
-hyps=root.findall("./turn/user_input/utterance/")
+audio_folders = []
+for n in all_data:
+    if 'audio' in n:
+        audio_folders.append(n)
+
+speech_files = []
+for n in all_data:
+    if 'speech' in n:
+        speech_files.append(n)
 
 
-callout=[]
-for hyp in hyps:
-    callout.append([word.text for word in hyp])
+def setfiles(self, index):
+    self.audio = os.listdir('/Users/admin/Desktop/datas/' + audio_folders[index])
+    self.speech = '/Users/admin/Desktop/datas/' + speech_files[index]
+    
+def setcalls(self, speech_file):
+    tree=ET.parse(speech_file)
+    root=tree.getroot()
+    self.utters=root.findall("./turn/user_input/")
+    self.hyps=root.findall("./turn/user_input/utterance/")
+
+
+class Trial():
+    def __init__(self):
+        self.index =0 
+        setfiles(self, self.index)
+        setcalls(self, self.speech)
+
+    def nexttrial(self):
+        self.index += 1
+        setfiles(self, self.index)
+        setcalls(self, self.speech)
+
+    def lasttrial(self):
+        self.index -= 1
+        setfiles(self, self.index)
+        setcalls(self, self.speech)
+
 
 def allwords(hypothesis):
     words=hypothesis[0].text
@@ -19,40 +48,35 @@ def allwords(hypothesis):
         words += " " + token.text
     return words
 
+trial=Trial()
 class CallOut:
     def __init__(self):
-        self.words=allwords(hyps[0])
-        self.confidence=hyps[0].attrib['confidence']
-        self.time=utters[0].attrib['start']
+        self.words=allwords(trial.hyps[0])
+        self.confidence=trial.hyps[0].attrib['confidence']
+        self.time=trial.utters[0].attrib['start']
         self.index = 0
 
     def nextcall(self):
-        self.index += 1
-        self.words = allwords(hyps[self.index])
-        self.confidence = hyps[self.index].attrib['confidence']
-        self.time = utters[self.index].attrib['start']
-
+        if self.index == len(trial.hyps) -1 :
+            trial.nexttrial()
+            self.__init__()
+        else:
+            self.index += 1
+            self.words = allwords(trial.hyps[self.index])
+            self.confidence = trial.hyps[self.index].attrib['confidence']
+            self.time = trial.utters[self.index].attrib['start']
+        
     def lastcall(self):
-        self.index -= 1
-        self.words = allwords(hyps[self.index])
-        self.confidence = hyps[self.index].attrib['confidence']
-        self.time = utters[self.index].attrib['start']
+        if self.index == 0:
+            trial.lasttrial()
+            self.__init__()
+        else:
+            self.index -= 1
+            self.words = allwords(trial.hyps[self.index])
+            self.confidence = trial.hyps[self.index].attrib['confidence']
+            self.time = trial.utters[self.index].attrib['start']
         
     def disp(self):
-        print Call.words
-        print "Time Stamp:", Call.time
-        print "Confidence:", Call.confidence
-
-
-def test():
-    Call=CallOut()
-    CallOut.disp(Call)
-    CallOut.nextcall(Call)
-    CallOut.disp(Call)
-    CallOut.lastcall(Call)
-    CallOut.disp(Call)
-
-
-
-
-
+        print self.words
+        print "Time Stamp:", self.time
+        print "Confidence:", self.confidence
